0a571552dbe31bdebbe57615f76a2abe4a99b8c9
aamir <aamir.pucit@gmail.com>
2016-06-06 10:40:14 +0400

    upgraded packages and before angularjs integration

---
+++ ManteqSecurityTest/ManteqSecurityTest/Controllers/ModuleController.cs
@@ -0,0 +1,21 @@
+﻿using ManteqSecurityTest.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManteqSecurityTest.Controllers
+{
+    [Authorize]
+    public class ModuleController : Controller
+    {
+        
+        [ManteqAuthorize("ManteqAdmin")]
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
